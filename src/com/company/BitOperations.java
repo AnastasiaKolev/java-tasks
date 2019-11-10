5ab6e65fe2826f138af7e2cdb2cbabc122aaaf6c
Anastasia <anastasia.kolevatykh@gmail.com>
2019-11-10 23:54:49 +0300

    added bit operations task

--- src/com/company/BitOperations.java
+++ src/com/company/BitOperations.java
@@ -32,6 +32,26 @@ public class BitOperations {
         return bitsCount;
     }
 
+    /**
+     * Напишите алгоритм, меняющий четные и нечетные биты местами в целом
+     * беззнаковом числе (тип uint32) используя минимальное количество операций.
+     * Пример
+     * input:  0011 1010 0101 1001 0110 1101 0110 1111
+     * output: 0011 0101 1010 0110 1001 1110 1001 1111
+     */
+    private static int pairwiseSwap(int num) {
+        int oddMask  = 0xAAAAAAAA; // 1010 1010 1010 1010 1010 1010 1010 1010
+        int evenMask = 0x55555555; // 0101 0101 0101 0101 0101 0101 0101 0101
+
+        oddMask = (num & oddMask); // занулили все четные биты
+        oddMask = oddMask >>> 1; // сдвинули нечетные биты на место четных
+
+        evenMask = (num & evenMask); // занулили все нечетные биты
+        evenMask = evenMask << 1;  // сдвинули четные биты на место нечетных
+
+        return oddMask | evenMask;   // соединяем сдвинутые биты
+    }
+
     public static void main(String[] args) {
         // given findNumber
         int[] arr = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 99, 10, 10, 11, 11, 12, 12};
@@ -42,10 +62,16 @@ public class BitOperations {
         // given countDiffBits
         int a = 34, b = 15;
 
-        // run findNumber
+        // run countDiffBits
         System.out.println( "a = " + a + " binary " + Integer.toBinaryString( a )
                 + "\nb = " + b + " binary " + Integer.toBinaryString( b )
                 + "\nDifferent bits count: " + countDiffBits( a, b ) );
+
+        // given pairwiseSwap
+        int num = 0xABCD9731;
+
+        // run pairwiseSwap
+        System.out.println("Pairwise Swap is: " + pairwiseSwap( num ) );
 
     }
 }
